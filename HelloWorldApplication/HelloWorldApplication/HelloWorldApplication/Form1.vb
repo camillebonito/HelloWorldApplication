7b7fd82646a170b1faaeb3226c4ef79e5b9e8205
Camille Bonito <camilleangelesbonito@yahoo.com>
2015-12-14 12:08:07 -0800

    HELLO WORLD
    Simple Hello World App!

---
+++ HelloWorldApplication/HelloWorldApplication/HelloWorldApplication/Form1.vb
@@ -0,0 +1,7 @@
+﻿Public Class HelloWorld
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        txtOutput.Text = "   HELLO WORLD!"
+
+    End Sub
+End Class
